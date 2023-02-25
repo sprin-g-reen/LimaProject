c6d51413bc44bf313f6074015e74dbc7161b7fdb
Vishal R <mrvishal2k2@gmail.com>
2023-02-25 20:47:02 +0530

    on my way

--- app.py
+++ app.py
@@ -12,9 +12,30 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["DEBUG"] = True
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+         print(request.form)
+         print("email:", request.form.get("Email"))
+         print("pass", request.form.get("Password"))
+        
+    return render_template('login.html')
+
+@app.route('/refer')
+def refer():
+    return render_template('referalP.html')
+
+@app.route('/broker')
+def broker():
+    return render_template('brokerP.html')
+
+@app.route('/creditform')
+def creditform():
+    return render_template('creditform.html')
 
 
 @app.route('/orders/1')
