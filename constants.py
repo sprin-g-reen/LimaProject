c02d9b091060058884b9d653f96771d46f2735c1
Vishal R <mrvishal2k2@gmail.com>
2023-03-12 22:06:47 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+# apiLoginId = "4d82dCKr6eG3"
+# transactionKey = "3jq6a3Gu3U9a3AQ6"
+# PRODUCTION = "https://api.authorize.net/xml/v1/request.api"
+
+apiLoginId = "7Df99pL7"
+transactionKey = "3kAa55jD2q2P32Vn"
